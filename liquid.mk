bb20ba4e0cb60ec7edac660539f3fe09e4d7642b
hedwig34 <hedwig34@gmail.com>
2014-12-15 21:31:35 -0500

    Unify L710 and L710T
    Change-Id: Ica44968abbb38f36b6d90d8d375046031122ab2c

--- liquid.mk
+++ liquid.mk
@@ -1,3 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
 $(call inherit-product, device/samsung/d2spr/full_d2spr.mk)
 
 # Enhanced NFC
@@ -9,9 +12,6 @@ $(call inherit-product, vendor/liquid/config/common_phone.mk)
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=d2spr \
     TARGET_DEVICE=d2spr \
-    BUILD_FINGERPRINT="samsung/d2spr/d2spr:4.4.2/KOT49H/L710VPUDND8:user/release-keys" \
-    PRIVATE_BUILD_DESC="d2spr-user 4.4.2 KOT49H L710VPUDND8 release-keys"
 
 PRODUCT_NAME := liquid_d2spr
 PRODUCT_DEVICE := d2spr
-
